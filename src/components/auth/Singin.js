e46b44eb18b09166e64ebee437967af1b572ac24
Lucas Manuel <lucas_sknr.010@live.com>
2021-01-26 15:43:04 -0300

    componentes de la aplicacion

---
+++ src/components/auth/Singin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Singin = () => {
+    return (  
+        <div>desde el registro</div>
+    );
+}
+ 
+export default Singin;
---
+++ src/components/dashboard/DashboarIndex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboarIndex = () => {
+    return ( 
+        <div>desde el dashboard</div>
+     );
+}
+ 
+export default DashboarIndex;
