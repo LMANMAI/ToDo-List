b68d356ed2011fa4da29693b3cdc1b6899db6781
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-11 01:06:03 -0300

    cambios en el Routing

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 //importo los componentes
 import Login from './components/auth/Login';
 import Singin from './components/auth/Singin';
-import DashboarIndex from './components/dashboard/DashboarIndex';
+import DashboardIndex from './components/dashboard/DashboardIndex';
 
 //importo los context
 import ProyectoState from './context/proyects/proyectoState';
+import TaskState from './context/task/taskState';
 function App() {
   return (
     <ProyectoState>
-      <Router>
-        <Switch>
-          <Route exact path="/" component ={Login} />
-          <Route exact path="/singin" component ={Singin} />
-          <Route exact path="/dashboard" component ={DashboarIndex} />
-        </Switch>
-      </Router>
+      <TaskState>
+        <Router>
+          <Switch>
+            <Route exact path="/" component ={Login} />
+            <Route exact path="/singin" component ={Singin} />
+            <Route exact path="/dashboard" component ={DashboardIndex} />
+          </Switch>
+        </Router>
+      </TaskState>
     </ProyectoState>
   );
 }
