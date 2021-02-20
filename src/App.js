f5e6f83bc5bd1aaa0d5715a12a95f391f2566a51
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-19 22:40:54 -0300

    Context de alertas y Autenticacion

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './components/auth/Login';
 import Singin from './components/auth/Singin';
 import DashboardIndex from './components/dashboard/DashboardIndex';
-
 //importo los context
 import ProyectoState from './context/proyects/proyectoState';
 import TaskState from './context/task/taskState';
+import AlertaState from './context/alertas/alertaState';
+import AuthState from './context/auth/authState';
+import tokenAuth from './config/tokenAuth';
+//Protego el componente
+import PrivateRoute from './routes/PrivateRoute';
+
 function App() {
+  //reviso que haya un token
+  const token = localStorage.getItem('token');
+  if(token){
+    tokenAuth(token);
+  }
   return (
     <ProyectoState>
       <TaskState>
-        <Router>
-          <Switch>
-            <Route exact path="/" component ={Login} />
-            <Route exact path="/singin" component ={Singin} />
-            <Route exact path="/dashboard" component ={DashboardIndex} />
-          </Switch>
-        </Router>
+        <AlertaState>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component ={Login} />
+                <Route exact path="/singin" component ={Singin} />
+                <PrivateRoute exact path="/dashboard" component ={DashboardIndex} />
+              </Switch>
+            </Router>
+          </AuthState>
+        </AlertaState>
       </TaskState>
     </ProyectoState>
   );
