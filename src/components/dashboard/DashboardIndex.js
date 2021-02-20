0fefc24a7f9c7361a64b06219c782b571dff0f0b
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-19 22:42:50 -0300

    Ruta privada, y recupera token del local storage

--- src/components/dashboard/DashboardIndex.js
+++ src/components/dashboard/DashboardIndex.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React,{ useContext, useEffect } from 'react'
 import SideBar from '../layout/SideBar';
 import TaskBody from '../task/TaskBody';
 import ProyectList from '../proyects/ProyectList';
+import ProyectoContext from '../../context/proyects/proyectoContext';
+import AuthContext from '../../context/auth/authContext';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const DashboardIndex = () => {
+    const proyectoContext = useContext(ProyectoContext);
+    const {panelproyecto, mensaje} = proyectoContext;
+
+    const authContext = useContext(AuthContext);
+    const { userAuth } = authContext;
+
+    const alertacontext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertacontext;
+
+    useEffect(()=>{
+      userAuth();
+      if(mensaje){ mostrarAlerta(mensaje.msg, mensaje.categoria) }
+
+    }, [mensaje]);
     return ( 
         <>
           <div className="dashboard_wraper">
+              {alerta ?(<div className={alerta.categoria}>{alerta.msg}</div>) :null}
                 <div className="glass_dashboard">
                     <SideBar/>     
-                    <ProyectList/>  
+                   <div className="wraper">
+                   {panelproyecto ? <ProyectList/> :null}
                     <TaskBody />
+                   </div>
                 </div>
             </div>
         </>
