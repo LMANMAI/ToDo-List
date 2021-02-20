7f4288231cab2e34436c10c0d6c77f3dea344161
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-19 22:35:02 -0300

    Conecte con BE

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,42 +1,87 @@
-import React from 'react';
+import React, {useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-const Login = () => {
+import AlertaContext from '../../context/alertas/alertaContext';
+import AuthContext from '../../context/auth/authContext';
+
+const Login = (props) => {
+    const authContext = useContext(AuthContext);
+    const { mensaje, autenticado, loginUser } = authContext;
+
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
+    useEffect(()=>{
+        if(autenticado){
+            props.history.push('/dashboard');
+        }
+        if(mensaje){
+            mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
+    },[mensaje, autenticado, props.history]);
+
+    const [ user, setUser ] = useState({
+        email:'',
+        password:''
+    });
+    const { email, password }= user;
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+    const handleSubmit = e =>{
+        e.preventDefault();
+        //valido que no tenga campos vacios
+        if(email.trim()==='' || password.trim()===''){
+            mostrarAlerta('Todos los campos son necesarios', 'alerta-error');
+            return;
+        }
+        loginUser(user);
+    }
     return ( 
-       <>       
         <div className="container">
             <div className="form_wraper">           
+            {alerta ?(<div className={alerta.categoria}>{alerta.msg}</div>) :null}
                <div className="form_container"> 
                <h2>Iniciar Sesion</h2>                  
-                   <form>
-                        <div className="input_container">
-                            <input 
-                                type="email"
-                                placeholder="Ingresa con tu Email"
-                            />
-                        </div>
+                   <form onSubmit={handleSubmit}>
+                       <div className="input_auth_container">
+                            <div className="input_container">
+                                <input 
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleChange}
+                                    placeholder="Ingresa con tu Email"
+                                />
+                            </div>
 
-                        <div className="input_container">
-                            <input 
-                                type="password" 
-                                placeholder="Contraseña"
-                            />
+                            <div className="input_container">
+                                <input 
+                                    type="password" 
+                                    name="password"
+                                    value={password}
+                                    onChange={handleChange}
+                                    placeholder="Contraseña"
+                                />
                         </div>
-
-                   </form>
+                    </div>
                    <div className="btn_container">
                         <Link 
                             className="btn btn_sec"
                             to="/singin"
                         >Obtener Cuenta</Link>
-                        <button 
-                            className="btn btn_primario"
-                        >Iniciar Sesion</button>                             
+                        <input 
+                            type="submit"
+                             className="btn btn_primario btn_auth"
+                             value="Iniciar Sesion"
+                        />                        
                     </div>
+                </form>
                </div>
-            </div>
-           
+            </div>          
         </div>
-       </>
        
      );
 }
--- src/components/auth/Singin.js
+++ src/components/auth/Singin.js
@@ -1,18 +1,77 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Singin = () => {
+const Singin = (props) => {
+    //contexts
+    const alertaContext = useContext(AlertaContext);
+    const {alerta, mostrarAlerta} = alertaContext;
+
+    const authContext = useContext(AuthContext);
+    const { mensaje, autenticado, registerUser } = authContext;
+
+    useEffect(()=>{
+        if(autenticado){
+            props.history.push('/dashboard');
+        }
+        if(mensaje){
+            mostrarAlerta( mensaje.msg, mensaje.categoria);
+        }
+        
+    },[autenticado, mensaje, props.history]);
+
+    const [ usern, setUserN ] = useState({
+        nombre:'',
+        email:'',
+        password:'',
+        confirmar: ''
+    });
+    const {nombre, email, password, confirmar} = usern;
+
+    const handleChange = e =>{
+        setUserN({
+            ...usern,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(nombre.trim() === '' || email.trim() ==='' || password.trim() === '' || confirmar.trim() ===''){
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+            return;
+        }
+        //pasword minimo de 6 caracteres
+        if(password.length < 6){
+            mostrarAlerta('el password debe ser de al menos 6 caracteres', 'alerta-error');
+            return;
+        }
+        //los dos passwords iguales
+        if(password.trim() !== confirmar.trim()){
+            mostrarAlerta('Los passwords deben ser iguales', 'alerta-error');
+            return;
+        }
+        //pasarlo al action
+        registerUser({
+            nombre,
+            email,
+            password
+        });
+    }
+    
     return (  
-        <>
-         <div className="container">
-         <div className="form_wraper">           
+        <div className="container">
+            <div className="form_wraper">     
+            {alerta ?(<div className={alerta.categoria}>{alerta.msg}</div>) :null}      
                 <div className="form_container"> 
                 <h2>Obtener una Cuenta</h2>                  
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="input_container">
                             <input 
                                 type="text"
                                 name="nombre"
+                                value={nombre}
+                                onChange={handleChange}
                                 placeholder="Nombre"
                             />
                          </div>
@@ -20,38 +79,45 @@ const Singin = () => {
                              <input 
                                  type="email"
                                  name="email"
+                                 value={email}
+                                 onChange={handleChange}
                                  placeholder="Email"
                              />
                          </div> 
                          <div className="input_container">
                              <input 
                                  type="password" 
+                                 name="password"
+                                 value={password}
+                                 onChange={handleChange}
                                  placeholder="Contraseña"
                              />
                          </div>
                          <div className="input_container">
                              <input 
                                  type="password" 
-                                 name="password"
+                                 name="confirmar"
+                                 value={confirmar}
+                                 onChange={handleChange}
                                  placeholder="Repetir Contraseña"
                              />
                          </div>
- 
+                        <div className="btn_container">
+                            <Link 
+                                className="btn btn_sec"
+                                to="/"
+                            >Ya tengo una Cuenta</Link>
+                            <input 
+                                type="submit"
+                                className="btn btn_primario btn_auth"
+                                value="Iniciar Sesion"
+                            />                                           
+                        </div>
                     </form>
-                    <div className="btn_container">
-                             <Link 
-                                 className="btn btn_sec"
-                                 to="/"
-                             >Ya tengo una Cuenta</Link>
-                             <button 
-                                 className="btn btn_primario"
-                             >Iniciar Sesion</button>                             
-                     </div>
                 </div>
-             </div>
-                    
+             </div>                   
          </div>
-        </>
+        
     );
 }
  
