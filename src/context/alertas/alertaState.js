a465d65630fbf73da3ae1c2aa06617ccbe03c786
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-19 22:36:50 -0300

    Context para las alertas

---
+++ src/context/alertas/alertaState.js
@@ -0,0 +1,42 @@
+import React, {useReducer} from 'react';
+
+//importo el reducer y el context
+import AlertaReducer from './alertaReducer';
+import AlertaContext from './alertaContext';
+//importo los types
+import { MOSTRAR_ALERTA, OCULTAR_ALERTA } from '../../types';
+
+const AlertaState = props => {
+
+    const initialState = {
+        //states inicial
+        alerta: null
+    }
+    //uso el reducer
+    const [state, dispatch ] = useReducer(AlertaReducer, initialState);
+    
+    const mostrarAlerta = (msg, categoria) =>{
+        dispatch({
+            type: MOSTRAR_ALERTA,
+            payload: {msg, categoria}
+        });
+        setTimeout(() => {
+            dispatch({
+                type: OCULTAR_ALERTA
+            });
+        }, 4000 );
+    }
+    return(
+        <AlertaContext.Provider
+            value={{
+                //paso las funciones y los states
+                alerta: state.alerta,
+                mostrarAlerta
+            }}
+        >
+            {props.children}
+        </AlertaContext.Provider>
+    )
+}
+ 
+export default AlertaState;
