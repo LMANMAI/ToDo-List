4c2d58cd57989ae6396c4b3edac7babb547e9ebf
Lucas Manuel <lucas_sknr.010@live.com>
2021-02-19 22:37:27 -0300

    Context para la autenticacion

---
+++ src/context/auth/authReducer.js
@@ -0,0 +1,44 @@
+//importo los types
+import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    OBTENER_USUARIO,
+    CERRAR_SESION
+} from '../../types';
+
+export default (state, action)=>{
+    switch (action.type) {
+        case REGISTRO_EXITOSO:
+            case LOGIN_EXITOSO:
+            localStorage.setItem('token', action.payload.token)
+            return{
+                ...state,
+                autenticado: true,
+                mensaje: null,
+                cargando: false
+        }
+        case OBTENER_USUARIO:
+            return{
+                ...state,
+                user: action.payload,
+                autenticado: true,
+                cargando: false
+            }
+        case CERRAR_SESION:
+        case LOGIN_ERROR:
+        case REGISTRO_ERROR:
+            localStorage.removeItem('token');
+            return{
+                ...state,
+                token: null,
+                user: null,
+                autenticado: null,
+                mensaje: action.payload,
+                cargando: false
+        }
+        default:
+           return state;
+    }
+}
